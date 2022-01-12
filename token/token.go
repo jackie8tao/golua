405ee9007b90b9b357c76b5539f1467a7866f6cd
jackietao <jackietao@tencent.com>
2022-01-12 19:39:14 +0800

    feat: add some tokens constances

--- token/token.go
+++ token/token.go
@@ -9,4 +9,49 @@ const (
 	ILLEGAL Token = iota
 	EOF
 	COMMENT
+
+	literal_beg
+	IDENT
+	INT
+	FLOAT
+	IMAG
+	CHAR
+	STRING
+	literal_end
+
+	keyword_beg
+	// Keywords
+	AND      // and
+	BREAK    // break
+	DO       // do
+	ELSE     // else
+	ELSEIF   // elseif
+	END      // end
+	FALSE    // false
+	FOR      // for
+	FUNCTION // function
+	GOTO     // goto
+	IF       // if
+	IN       // in
+	LOCAL    // local
+	NIL      // nil
+	NOT      // not
+	OR       // or
+	REPEAT   // repeat
+	RETURN   // return
+	THEN     // then
+	TRUE     // true
+	UNTIL    // until
+	WHILE    // while
+	keyword_end
+
+	operator_beg
+	// Operators and delimiters
+	ADD // +
+	SUB // -
+	MUL // *
+	QUO // /
+	REM // %
+
+	operator_end
 )
