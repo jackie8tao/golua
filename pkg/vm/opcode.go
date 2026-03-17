81728ed8aa4a9c1ee0e088577de4b6f1aa1f0512
jackietao <jackie8tao@outlook.com>
2026-03-17 18:46:25 +0800

    feat: support global variable

--- pkg/vm/opcode.go
+++ pkg/vm/opcode.go
@@ -3,11 +3,13 @@ package vm
 type OpCode uint8
 
 const (
-	OpAdd OpCode = iota + 128 // +
-	OpSub                     // -
-	OpMul                     // *
-	OpDiv                     // /
-	OpPow                     // ^
-	OpConstant
-	OpPrint // for debugging
+	OpAdd       OpCode = iota + 128 // +
+	OpSub                           // -
+	OpMul                           // *
+	OpDiv                           // /
+	OpPow                           // ^
+	OpConstant                      // load constant
+	OpGetGlobal                     // get global value
+	OpSetGlobal                     // set global value
+	OpPrint                         // for debugging
 )
--- pkg/vm/vm.go
+++ pkg/vm/vm.go
@@ -7,24 +7,43 @@ import (
 
 const defaultStackSize = 1024
 
+type binaryOpHandler func(v1, v2 LValue) (LValue, error)
+
 type Vm struct {
 	stack     []LValue
 	codes     []OpCode
 	constants []LValue
+	globals   map[string]LValue
 	base      uint // stack base
 	top       uint // stack top
 	pc        uint // program counter
+
+	// extra utils files
+	binaryOpTable map[OpCode]binaryOpHandler
 }
 
 func NewVm() *Vm {
-	return &Vm{
+	vm := &Vm{
 		stack:     make([]LValue, defaultStackSize),
 		codes:     make([]OpCode, 0),
 		constants: make([]LValue, 0),
+		globals:   make(map[string]LValue),
 		base:      0,
 		top:       0,
 		pc:        0,
 	}
+	vm.binaryOpTable = map[OpCode]binaryOpHandler{
+		OpAdd: vm.opAdd,
+		OpSub: vm.opSub,
+		OpMul: vm.opMul,
+		OpDiv: vm.opDiv,
+		OpPow: vm.opPow,
+	}
+	return vm
+}
+
+func (v *Vm) WriteGlobals(key string, val LValue) {
+	v.globals[key] = val
 }
 
 func (v *Vm) WriteConstant(val LValue) uint8 {
@@ -45,62 +64,59 @@ func (v *Vm) Execute() error {
 			break
 		}
 		switch v.codes[v.pc] {
+		case OpPrint: // only used for debugging
+			val := v.stPop()
+			fmt.Println(val.String())
+			v.pc++
 		case OpConstant:
 			v.pc++
-			idx := v.codes[v.pc]
-			if int(idx) >= len(v.constants) {
+			idx := int(v.codes[v.pc])
+			if idx >= len(v.constants) {
 				return fmt.Errorf("invalid constant index")
 			}
 			v.stPush(v.constants[idx])
 			v.pc++
-		case OpAdd:
+		case OpAdd, OpSub, OpMul, OpDiv, OpPow:
+			handler, ok := v.binaryOpTable[v.codes[v.pc]]
+			if !ok {
+				return fmt.Errorf("cannot find binary operator handler")
+			}
 			v1 := v.stPop()
 			v2 := v.stPop()
-			res, err := v.opAdd(v1, v2)
+			res, err := handler(v1, v2)
 			if err != nil {
 				return err
 			}
 			v.stPush(res)
 			v.pc++
-		case OpSub:
-			v1 := v.stPop()
-			v2 := v.stPop()
-			res, err := v.opSub(v1, v2)
-			if err != nil {
-				return err
-			}
-			v.stPush(res)
+		case OpGetGlobal:
 			v.pc++
-		case OpMul:
-			v1 := v.stPop()
-			v2 := v.stPop()
-			res, err := v.opMul(v1, v2)
-			if err != nil {
-				return err
+			idx := int(v.codes[v.pc])
+			if idx >= len(v.constants) {
+				return fmt.Errorf("invalid constant index")
 			}
-			v.stPush(res)
-			v.pc++
-		case OpDiv:
-			v1 := v.stPop()
-			v2 := v.stPop()
-			res, err := v.opDiv(v1, v2)
-			if err != nil {
-				return err
+			if v.constants[idx].Type() != LTString {
+				return fmt.Errorf("invalid global name")
 			}
-			v.stPush(res)
-			v.pc++
-		case OpPow:
-			v1 := v.stPop()
-			v2 := v.stPop()
-			res, err := v.opPow(v1, v2)
-			if err != nil {
-				return err
+			key := v.constants[idx].(*LString).Value
+			val, ok := v.globals[key]
+			if !ok {
+				return fmt.Errorf("invalid global variable: %s", key)
 			}
-			v.stPush(res)
+			v.stPush(val)
 			v.pc++
-		case OpPrint:
+		case OpSetGlobal:
+			v.pc++
+			idx := int(v.codes[v.pc])
+			if idx >= len(v.constants) {
+				return fmt.Errorf("invalid constant index")
+			}
+			if v.constants[idx].Type() != LTString {
+				return fmt.Errorf("invalid global name")
+			}
+			key := v.constants[idx].(*LString).Value
 			val := v.stPop()
-			fmt.Println(val.String())
+			v.globals[key] = val
 			v.pc++
 		default:
 			panic("invalid opcode")
